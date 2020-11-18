fadabc785087be22ad2f60fc62be88330fa45485
dimples <2890841438@qq.com>
2020-11-18 21:42:34 +0800

    react-redux

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import Mine from './pages/Mine';
+import Nav from './components/Nav'
+import UCenter from './pages/UCenter'
+import NotFound from './pages/NotFound'
+import Demo from './pages/Demo'
+import Book from './pages/Book'
+import JavaBook from './pages/JavaBook'
+import WebBook from './pages/WebBook'
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Redirect from='/hellomine' to='/mine'></Redirect>
+          <Route exact path='/home' component={Home} ></Route>
+          <Route exact strict path='/mine' component={Mine} ></Route>
+          <Route exact strict path='/mine/ucenter/:id?/:name?' component={UCenter}></Route>
+          <Route path='/demo' render={(props) => <Demo {...props} name="你好" />}></Route>
+          <Book path='/book'>
+            <Switch>
+              <Route path='/book/javabook' component={JavaBook}></Route>
+              <Route path='/book/webbook' component={WebBook}></Route>
+            </Switch>
+          </Book>
+
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
